0511c6828b94cfe5e52880dd06600e246a16c932
jahdieltorreglosa-sudo <jahdiel.torreglosa@udea.edu.co>
2025-10-17 05:33:44 -0500

    ultima version unificada

--- lab3/1Y2unificado.cpp
+++ lab3/1Y2unificado.cpp
@@ -3,20 +3,20 @@
 #include <string>
 using namespace std;
 
-//parte 1
-
 string texto_a_binario(string texto) {
     string binario = "";
     for (int i = 0; i < texto.size(); i++) {
         unsigned char c = texto[i];
         for (int j = 7; j >= 0; j--) {
-            if (c & (1 << j)) binario += "1";
-            else binario += "0";
+            if (c & (1 << j)) {
+                binario += "1";
+            } else {
+                binario += "0";
+            }
         }
     }
     return binario;
 }
-
 
 string metodo1(string bits, int n) {
     string salida = "";
@@ -24,28 +24,54 @@ string metodo1(string bits, int n) {
         string bloque = bits.substr(i, n);
         if (i == 0) {
             for (int j = 0; j < bloque.size(); j++) {
-                salida += (bloque[j] == '0') ? '1' : '0';
+                if (bloque[j] == '0') {
+                    salida += '1';
+                } else {
+                    salida += '0';
+                }
             }
         } else {
             string anterior = salida.substr(i - n, n);
             int unos = 0, ceros = 0;
             for (int j = 0; j < anterior.size(); j++) {
-                if (anterior[j] == '1') unos++;
-                else ceros++;
+                if (anterior[j] == '1') {
+                    unos++;
+                } else {
+                    ceros++;
+                }
             }
+
             if (unos == ceros) {
                 for (int j = 0; j < bloque.size(); j++) {
-                    salida += (bloque[j] == '0') ? '1' : '0';
+                    if (bloque[j] == '0') {
+                        salida += '1';
+                    } else {
+                        salida += '0';
+                    }
                 }
             } else if (ceros > unos) {
                 for (int j = 0; j < bloque.size(); j++) {
-                    if ((j % 2) == 0) salida += (bloque[j] == '0') ? '1' : '0';
-                    else salida += bloque[j];
+                    if (j % 2 == 0) {
+                        if (bloque[j] == '0') {
+                            salida += '1';
+                        } else {
+                            salida += '0';
+                        }
+                    } else {
+                        salida += bloque[j];
+                    }
                 }
             } else {
                 for (int j = 0; j < bloque.size(); j++) {
-                    if ((j % 3) == 0) salida += (bloque[j] == '0') ? '1' : '0';
-                    else salida += bloque[j];
+                    if (j % 3 == 0) {
+                        if (bloque[j] == '0') {
+                            salida += '1';
+                        } else {
+                            salida += '0';
+                        }
+                    } else {
+                        salida += bloque[j];
+                    }
                 }
             }
         }
@@ -66,12 +92,10 @@ string metodo2(string bits, int n) {
     }
     return salida;
 }
-// parte 2
-
 
 string binario_a_texto(string bits) {
     string texto = "";
-    for (int i = 0; i + 7 < bits.size(); i += 8) {
+    for (int i = 0; i + 8 <= bits.size(); i += 8) {
         int valor = 0;
         for (int j = 0; j < 8; j++) {
             valor = valor * 2 + (bits[i + j] - '0');
@@ -81,44 +105,94 @@ string binario_a_texto(string bits) {
     return texto;
 }
 
-string metodo1_inverso
+string metodo1_inverso(string bits, int n) {
+    return metodo1(bits, n);
+}
 
 string metodo2_inverso(string bits, int n) {
     string salida = "";
-    for //(int i = 0; i
-
-//parte 3
+    for (int i = 0; i < bits.size(); i += n) {
+        string bloque = bits.substr(i, n);
+        if (bloque.size() > 0) {
+            string nuevo = "";
+            for (int j = 1; j < bloque.size(); j++) {
+                nuevo += bloque[j];
+            }
+            nuevo += bloque[0];
+            salida += nuevo;
+        }
+    }
+    return salida;
+}
 
 int main() {
-    int opcion;
-    cout << "Elige una opción:" << endl;
-    cout << "1. Codificar archivo" << endl;
-    cout << "2. Decodificar archivo" << endl;
-    cout << "Opción: ";
+    int opcion, metodo, semilla;
+    string archivo_entrada, archivo_salida;
+
+    cout << "1. Codificar" << endl;
+    cout << "2. Decodificar" << endl;
+    cout << "Elija una opcion: ";
     cin >> opcion;
 
-    string archivo_entrada, archivo_salida;
-    int semilla, metodo;
+    cout << "Ingrese la semilla: ";
+    cin >> semilla;
 
+    cout << "Ingrese el metodo (1 o 2): ";
+    cin >> metodo;
+
+    cout << "Ingrese el nombre del archivo de entrada: ";
+    cin >> archivo_entrada;
+
+    cout << "Ingrese el nombre del archivo de salida: ";
+    cin >> archivo_salida;
+
+    ifstream entrada(archivo_entrada);
+    if (!entrada.is_open()) {
+        cout << "No se pudo abrir el archivo de entrada" << endl;
+        return 0;
+    }
+
+    string contenido = "", linea;
+    while (getline(entrada, linea)) {
+        if (opcion == 1) {
+            contenido += linea + "\n";
+        } else {
+            contenido += linea;
+        }
+    }
+    entrada.close();
+
+    if (opcion == 1 && !contenido.empty()) {
+        contenido.pop_back();
+    }
+
+    string resultado;
     if (opcion == 1) {
-        cout << "escribe el nombre del archivo de entrada: ";
-        cin >> archivo_entrada;
-        cout << "escribe el nombre del archivo de salida: ";
-        cin >> archivo_salida;
-        cout << "dime la semilla (n): ";
-        cin >> semilla;
-        cout << "dime el metodo (1 o 2): ";
-        cin >> metodo;
+        string binario = texto_a_binario(contenido);
+        if (metodo == 1) {
+            resultado = metodo1(binario, semilla);
+        } else {
+            resultado = metodo2(binario, semilla);
+        }
+    } else {
+        string decodificado;
+        if (metodo == 1) {
+            decodificado = metodo1_inverso(contenido, semilla);
+        } else {
+            decodificado = metodo2_inverso(contenido, semilla);
+        }
+        resultado = binario_a_texto(decodificado);
+    }
 
-  else if (opcion == 2) {
-        cout << "escribe el archivo que vas a decodificar: ";
-        cin >> archivo_entrada;
-        cout << "escribe el archivo donde quieres guardar: ";
-        cin >> archivo_salida;
-        cout << "dime la semilla: ";
-        cin >> semilla;
-        cout << "dime el metodo (1 o 2): ";
-        cin >> metodo;
+    ofstream salida(archivo_salida);
+    if (!salida.is_open()) {
+        cout << "No se pudo abrir el archivo de salida" << endl;
+        return 0;
+    }
+    salida << resultado;
+    salida.close();
 
-	ifstream in(archivo_entrada.c_str());
-        if (!in.is_open()) {
+    cout << "Operacion completada." << endl;
+
+    return 0;
+}
